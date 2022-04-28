9a96c58a9afe309725720c4cadf19452b66679c8
Thiago Silva <euthiagosrodrigues@gmail.com>
2022-04-28 20:51:19 -0300

    alteracao menu mobile

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-let button = document.getElementById("menu-list-responsive");
+let menuMobile= document.getElementById("menu-list-mobile");
+let menuNormal = document.getElementById("menu-list");
+let buttonNav = document.getElementById("button-navbar");
+let buttonImg = document.getElementById("button-img")
+
+
 let stats = false; 
+let menu = true;
 
 function AbrirJanela() {
     if(stats == false ) {
-        button.style.visibility = "visible";
+        menuMobile.style.visibility = "visible";
         stats = true;
-    }  else {
-        button.style.visibility = "hidden";
+    } else {
+        menuMobile.style.visibility = "hidden";
         stats = false;
-
     } 
 }   
+
+function updateImg() {
+    if(menuMobile.style.visibility == "visible") {
+        buttonImg.src = "/_img/excluir.png"
+    } else {
+        buttonImg.src = "/_img/cardapio.png"
+    }
+}
+
+
+buttonNav.addEventListener("click", AbrirJanela)
+buttonNav.addEventListener("click", updateImg)
+
+
+
+
